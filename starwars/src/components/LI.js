f6975841862d94d991c4f962d5ddf6c238898870
Robert Pedersen <rob84044@users.noreply.github.com>
2019-05-18 11:14:03 -0600

    Added Li.js file
    This file was added so I could specify which data the Unorded list would display within each placard that will eventually be built/styled

---
+++ starwars/src/components/LI.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './StarWars.css';
+
+const Li = props => {
+  return (
+    <div className="charInfo">
+      <ul className="charData">
+        <li className="li-item">{props.starwarsChars.name}</li>
+        <li className="li-item">{props.starwarsChars.gender}</li>
+        <li className="li-item">{props.starwarsChars.birth_year}</li>
+        <li className="li-item">{props.starwarsChars.eye_color}</li>
+        <li className="li-item">{props.starwarsChars.hair_color}</li>
+        <li className="li-item">{props.starwarsChars.height}</li>
+        <li className="li-item">{props.starwarsChars.mass}</li>
+        <li className="li-item">{props.starwarsChars.homeworld}</li>
+        <li className="li-item">{props.starwarsChars.skin_color}</li>
+        {/* If you complete the project, come back and add in the additional array values*/}
+        {/* <li className="li-item">{props.starwarsChars.name}</li>
+        <li className="li-item">{props.starwarsChars.name}</li>
+        <li className="li-item">{props.starwarsChars.name}</li>
+        <li className="li-item">{props.starwarsChars.name}</li>
+        <li className="li-item">{props.starwarsChars.name}</li> */}
+      </ul>
+    </div>
+  );
+};
+
+export default Li;

bc56426c9aaa73e1c284b45e715c47954dd21003
Robert Pedersen <rob84044@users.noreply.github.com>
2019-05-18 11:53:02 -0600

    Updated the li-data file to be more presentable and easier to read. Still attempting to structure the li-item file

--- starwars/src/components/LI-data.js
+++ starwars/src/components/LI-data.js
@@ -5,15 +5,60 @@ const LiData = props => {
   return (
     <div className="charInfo">
       <ul className="charData">
-        <li className="li-item">{props.starwarsChars.name}</li>
-        <li className="li-item">{props.starwarsChars.gender}</li>
-        <li className="li-item">{props.starwarsChars.birth_year}</li>
-        <li className="li-item">{props.starwarsChars.eye_color}</li>
-        <li className="li-item">{props.starwarsChars.hair_color}</li>
-        <li className="li-item">{props.starwarsChars.height}</li>
-        <li className="li-item">{props.starwarsChars.mass}</li>
-        <li className="li-item">{props.starwarsChars.homeworld}</li>
-        <li className="li-item">{props.starwarsChars.skin_color}</li>
+        <li className="li-item">
+          <p className="desc">
+            <strong>Name:</strong>
+          </p>
+          {props.starwarsChars.name}
+        </li>
+        <li className="li-item">
+          <p className="desc">
+            <strong>Gender:</strong>
+          </p>
+          {props.starwarsChars.gender}
+        </li>
+        <li className="li-item">
+          <p className="desc">
+            <strong>Birth Year:</strong>
+          </p>
+          {props.starwarsChars.birth_year}
+        </li>
+        <li className="li-item">
+          <p className="desc">
+            <strong>Eye Color:</strong>
+          </p>
+          {props.starwarsChars.eye_color}
+        </li>
+        <li className="li-item">
+          <p className="desc">
+            <strong>Hair Color:</strong>
+          </p>
+          {props.starwarsChars.hair_color}
+        </li>
+        <li className="li-item">
+          <p className="desc">
+            <strong>Height:</strong>
+          </p>
+          {props.starwarsChars.height}
+        </li>
+        <li className="li-item">
+          <p className="desc">
+            <strong>Weight:</strong>
+          </p>
+          {props.starwarsChars.mass}
+        </li>
+        <li className="li-item">
+          <p className="desc">
+            <strong>Home World:</strong>
+          </p>
+          {props.starwarsChars.homeworld}
+        </li>
+        <li className="li-item">
+          <p className="desc">
+            <strong>Skin Color:</strong>
+          </p>
+          {props.starwarsChars.skin_color}
+        </li>
         {/* If you complete the project, come back and add in the additional array values*/}
         {/* <li className="li-item">{props.starwarsChars.name}</li>
         <li className="li-item">{props.starwarsChars.name}</li>
